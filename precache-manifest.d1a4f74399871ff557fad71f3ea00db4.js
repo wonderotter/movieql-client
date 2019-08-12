6f93e1166b999a7828a040651aed94b46fd5ba68
wonderotter <gouk32@naver.com>
2019-08-13 01:44:45 +0900

    Updates

---
+++ precache-manifest.d1a4f74399871ff557fad71f3ea00db4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e92ec1cb378763c95a9ea3f6238f73dd",
+    "url": "/movieql-client/index.html"
+  },
+  {
+    "revision": "185ebe696ed4860f4fd1",
+    "url": "/movieql-client/static/js/2.0682edf0.chunk.js"
+  },
+  {
+    "revision": "f9415ac3a521a2b6d697",
+    "url": "/movieql-client/static/js/main.c9fe24d0.chunk.js"
+  },
+  {
+    "revision": "be9c2221444aafaae974",
+    "url": "/movieql-client/static/js/runtime~main.f5d7f0ca.js"
+  }
+]);
