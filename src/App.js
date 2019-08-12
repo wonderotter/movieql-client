049f1b269676dbe94dcd9e22ace64a3511e2487a
wonderotter <gouk32@naver.com>
2019-08-12 15:24:48 +0900

    Setting up React Router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import client from './apolloClient';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import {ApolloProvider} from 'react-apollo';
+import client from './apolloClient';
+import Home from './Home';
+import Detail from './Detail';
 
 class App extends React.Component {
   render(){
     return (
-      <ApolloProvider client={client}>
-      <div className="App"/>
-      </ApolloProvider>
+      <Router basename="/">
+        <ApolloProvider client={client}>
+          <Route exact={true} path={"/"} component={Home} />
+          <Route path={"/details/:movieId"} component={Detail} />
+        </ApolloProvider>
+      </Router>
     );
   }
 }
