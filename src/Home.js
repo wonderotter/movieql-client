046723729cf7b4fe78f11be8355612ebab011d21
wonderotter <gouk32@naver.com>
2019-08-12 16:04:43 +0900

    Getting data from the GraphQL API

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,13 @@
 import React from "react";
+import {Query} from 'react-apollo';
+import { HOME_PAGE } from './queries';
 
-const Home = () => <h1>Home</h1>;
+const Home = () => <Query query={HOME_PAGE}>{({ loading, data, error}) => {
+    if(loading) return <span>Loading</span>;
+    if(error) return <span>Error</span>;
+    if(data){
+        return data.movies.map(movie => <h2 key={movie.id}>{movie.title_long} / {movie.rating}</h2>);
+    }
+}}</Query>;
 
 export default Home;
---
+++ src/queries.js
@@ -0,0 +1,12 @@
+import gql from 'graphql-tag';
+
+export const HOME_PAGE = gql`
+    query {
+        movies(genre: "Romance", rating: 7, limit: 50){
+            id
+            title_long
+            rating
+            genres
+            }
+        }
+`;
